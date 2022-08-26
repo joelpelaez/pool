ebc281f1c20d5f22ccda5dd8eec1c1f6317b9fed
Joel Pelaez Jorge <joel.pelaez.jorge@gmail.com>
2022-08-25 21:09:38 -0500

    Fix compilation with MSVC with Windows headers
    When the boost::pool is instantiated and the code is compiled with
    MSVC compiler, it can fail if Windows specific headers are included,
    adding the max and min macros.
    
    Because some part of the header file already has preprocessor guards,
    this change add same solution to call to std::numeric_limits<>::max()
    to avoid any issues.
    
    On MSVC using C++20 standard mode, the header file also fires the same
    issue, only with include it on code file, without require object
    instantiation.

---
+++ test/test_issue_49.cpp
@@ -0,0 +1,17 @@
+/* Copyright (C) 2022 Joel Pelaez Jorge
+*
+* Use, modification and distribution is subject to the
+* Boost Software License, Version 1.0. (See accompanying
+* file LICENSE_1_0.txt or http://www.boost.org/LICENSE_1_0.txt)
+*/
+
+// Test of issue #49 (https://github.com/boostorg/pool/issues/49)
+
+#include <Windows.h>
+#include <boost/pool/pool.hpp>
+
+int main()
+{
+  // no require instantiation
+  return 0;
+}
